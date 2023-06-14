70408e9eacb37388dfaec6a4592239762d1d114b
Gustavo Assis <assiscs2@gmail.com>
2023-06-14 20:25:36 -0300

    changes to the video player

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,9 +1,11 @@
 export function VideoPlayer() {
     return (
-        <div>
+        <div className="mt-4 p-2 mb-2">
             <video 
             src="https://firebasestorage.googleapis.com/v0/b/maloni-gold-landing-page.appspot.com/o/videos%2Fmaloni-gold-video.mp4?alt=media&token=4c07cbb5-c58a-445c-8f22-a35bc8f60ec5"
-            controls={true}
+            controls
+            autoPlay
+            muted
             className="min-w-full min-h-full w-auto h-auto bg-cover"
             ></video>
         </div>
