839bf7c4d8983b7dcfec73092b7374243ac9cc98
Gustavo Assis <assiscs2@gmail.com>
2023-06-29 15:31:49 -0300

    changes to the video

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -2,7 +2,7 @@ export function VideoPlayer() {
     return (
         <div className="mt-4 p-2 mb-2">
             <video 
-            src="https://firebasestorage.googleapis.com/v0/b/maloni-gold-landing-page.appspot.com/o/videos%2Fmaloni-gold-video.mp4?alt=media&token=4c07cbb5-c58a-445c-8f22-a35bc8f60ec5"
+            src="https://firebasestorage.googleapis.com/v0/b/maloni-gold-landing-page.appspot.com/o/videos%2Fvideo-maloni-gold.MOV?alt=media&token=010fb61e-0446-4635-8b37-65554485ac6d"
             controls
             className="min-w-full min-h-full w-auto h-auto bg-cover"
             ></video>
