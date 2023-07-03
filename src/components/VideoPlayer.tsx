55d6aa7341ff8136d3511a09c59e40b9091f324e
Gustavo Assis <assiscs2@gmail.com>
2023-07-03 14:39:23 -0300

    changes to video player

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -5,7 +5,6 @@ export function VideoPlayer() {
             src="https://firebasestorage.googleapis.com/v0/b/maloni-gold-landing-page.appspot.com/o/videos%2Fvideo-maloni-gold-compressed-720p.mp4?alt=media&token=54ee7af7-52af-416a-848d-26b3c48165a8"
             controls
             className="min-w-full min-h-full w-auto h-auto bg-cover lg:max-w-[480px]"
-            preload="auto"
             ></video>
         </div>
     )
